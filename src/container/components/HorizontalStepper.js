31d618cada77db28c4688ef1880f76427eb41b4e
Matteo <matteo3.@scaccabarozzi@mail.polimi.it>
2021-11-27 20:09:21 +0100

    Buy button arrow and eliminate step text

--- src/container/components/HorizontalStepper.js
+++ src/container/components/HorizontalStepper.js
@@ -14,7 +14,7 @@ import 'react-day-picker/lib/style.css';
 import "./HorizontalStepper.css";
 import OptionalProductsCards from './OptionalProductsCards';
 import Riepilogo from './Riepilogo';
-import Login from '../Login';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import FinalStepButton from './finalStepButton';
 import { useNavigate } from "react-router";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -188,8 +188,6 @@ axios.post("/order/create",
         </React.Fragment>
       ) : (
         <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
             <Button
               color="inherit"
@@ -197,15 +195,11 @@ axios.post("/order/create",
               onClick={handleBack}
               sx={{ mr: 1 }}
               variant="outlined"
+              startIcon={<ArrowBackIosNewIcon />}
             >
               Back
             </Button>
             <Box sx={{ flex: '1 1 auto' }} />
-            {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                Skip
-              </Button>
-            )}
             {activeStep !== steps.length - 1 ? 
             <Button onClick={handleNext} variant="contained" endIcon={<ArrowForwardIosIcon />} >
               Next
